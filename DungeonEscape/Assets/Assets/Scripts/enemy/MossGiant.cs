c6d3b4b4ffc7b8a868226437f2c91476d93087d3
dpolozov <dpolozov@gmail.com>
2020-10-07 15:21:30 -0700

    add skeleton enemy. convert to abstract enemy class

--- DungeonEscape/Assets/Assets/Scripts/enemy/MossGiant.cs
+++ DungeonEscape/Assets/Assets/Scripts/enemy/MossGiant.cs
@@ -4,53 +4,5 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    private Vector3 currentTarget;
-    private Animator anim;
-    private SpriteRenderer mossSprite;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        anim = GetComponentInChildren<Animator>();
-        mossSprite = GetComponentInChildren<SpriteRenderer>();
-        currentTarget = pointB.position;
-
-    }
-
-    public override void Update()
-    {
-
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            return;
-        }
-
-        Movement();
-
-    }
-
-    void Movement()
-    {
-        if (currentTarget == pointA.position)
-        {
-            mossSprite.flipX = true;
-        }
-        else
-        {
-            mossSprite.flipX = false;
-        }
-
-        if (transform.position == pointA.position)
-        {
-            currentTarget = pointB.position;
-            anim.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position)
-        {
-            currentTarget = pointA.position;
-            anim.SetTrigger("Idle");
-        }
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
-    }
 
 }
